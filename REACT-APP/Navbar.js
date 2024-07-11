ba5bf70e430cfbe107d5e34361947652f7d9b2df
shreyayadav08 <shreyapabbala@gmail.com>
2024-07-11 20:30:13 +0530

    initial commit

---
+++ REACT-APP/Navbar.js
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return (
+    
+    <div className="navbar">
+    <a href="#"> Contact Us </a>
+    <a href="#">About Us| </a>
+    <a href="#">Home |</a>
+    </div>
+    
+    );
+    }
+    export default Navbar;
---
+++ REACT-APP/Sidebar.js
@@ -0,0 +1,12 @@
+const Sidebar = () => {
+    return (
+    
+    <div className="sidebabr">
+    <span>
+    Category inforamtion
+    Sidebar
+    </span>
+    </div>
+    );
+    }
+    export default Sidebar;
